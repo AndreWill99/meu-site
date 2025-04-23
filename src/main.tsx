e98c5894cc70e56d1f0d51c4478182536a473757
AndreWill99 <andrewilliam.cbpm@gmail.com>
2025-04-23 12:28:33 -0300

    importações ts

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@emotion/react';
-import theme from './theme.ts';
+import theme from './theme';
 import './Background.css';
 
 createRoot(document.getElementById('root')!).render(
