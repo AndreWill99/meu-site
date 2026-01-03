85384c76ab35c3af8adff035a103fd98752f3933
AndreWill99 <andrewilliam.cbpm@gmail.com>
2026-01-03 01:42:25 -0300

    Correção package.json e Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,33 +4,33 @@ import '../Styles/header.css';
 
 const Header = () => {
   return (
-    <header class="header_container">
+    <header className="header_container">
       
-      <div class="foto_container">
+      <div className="foto_container">
         <img src="src/assets/foto perfil.jpg" alt="Foto perfil André" />
       </div>
 
-      <div class="info_container">
-        <div class="profile_infos">
+      <div className="info_container">
+        <div className="profile_infos">
           <h1>André William</h1>
           <p>UX/UI Designer | Front-End</p>
         </div>
 
-        <div class="social_icones">
-          <img src="src\assets\behance CV logo.png" class="bh_logo" alt="Behance logo"
-            onclick="location.href='https://www.behance.net/andrwcabral'" />
+        <div className="social_icones">
+          <img src="src\assets\behance CV logo.png" className="bh_logo" alt="Behance logo"
+            onClick={() => window.location.href='https://www.behance.net/andrwcabral'} />
             
-          <img src="src/assets/github cv logo.png" class="gh_logo" alt="Github logo"
-            onclick="location.href='https://github.com/AndreWill99'" />
+          <img src="src/assets/github cv logo.png" className="gh_logo" alt="Github logo"
+            onClick={() => window.location.href='https://github.com/AndreWill99'} />
 
-          <img src="src/assets/linkedin cv logo.png" class="ld_logo" alt="Linkedin"
-            onclick="location.href='https://www.linkedin.com/in/andre-william-costa-33b873172/'" />
+          <img src="src/assets/linkedin cv logo.png" className="ld_logo" alt="Linkedin"
+            onClick={() => window.location.href='https://www.linkedin.com/in/andre-william-costa-33b873172/'} />
 
-          <img src="src/assets/wpp cv logo.png" class="wpp_logo" alt="Whatsapp"
-            onclick="location.href='https://wa.me/+554198452031'" />
+          <img src="src/assets/wpp cv logo.png" className="wpp_logo" alt="Whatsapp"
+            onClick={() => window.location.href='https://wa.me/+554198452031'} />
 
-          <img src="src/assets/email cv logo.png" class="email_logo" alt="email logo"
-            onclick="location.href='mailto: andrewilliam2012@hotmail.com'"/>
+          <img src="src/assets/email cv logo.png" className="email_logo" alt="email logo"
+            onClick={() => window.location.href='mailto: andrewilliam2012@hotmail.com'}/>
         </div>
       </div>
     </header>
